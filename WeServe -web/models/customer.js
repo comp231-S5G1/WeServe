9a2e63564220b2eb1aa2840d23f5f645fb3bfb14
Bhargav Marsonia <71554677+balapate123@users.noreply.github.com>
2020-11-19 13:31:53 -0800

    Update customer.js
    Added some comments

--- WeServe -web/models/customer.js
+++ WeServe -web/models/customer.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 var passportLocalMongooseCustomer = require("passport-local-mongoose");
-// adding a comment to see push demos
+// adding a comment to see push demos and more
 var CustomerSchema = new mongoose.Schema({
 
 	isCustomer: {
